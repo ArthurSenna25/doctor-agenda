d31502fb7bfe489427325b11ec465a283a302f9e
Arthur de Senna <sennaa.dev@gmail.com>
2025-06-03 18:28:32 -0300

    feat: redirect user to dashboard on home page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return <Button>Senna Dev - Doutor Agenda!</Button>;
   redirect("/dashboard");
 }
